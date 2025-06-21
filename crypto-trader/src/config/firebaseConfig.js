ed6b0959630cc7b6558290d40e6369065be6a031
Prakhar Shukla <prakhars.cse22@sbjit.edu.in>
2025-06-21 17:04:31 +0530

    Add files via upload

---
+++ crypto-trader/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZla1ZxMtgRYcb_dPRjbR0yo41i6cUrhc",
+  authDomain: "crypto-hunter-aef80.firebaseapp.com",
+  projectId: "crypto-hunter-aef80",
+  storageBucket: "crypto-hunter-aef80.firebasestorage.app",
+  messagingSenderId: "990678404093",
+  appId: "1:990678404093:web:f8f5089dec46e71e8ee794"
+};
+
+export default firebaseConfig;
