9ae35b727a32d0509952c165832b8d356ed3d8c8
Prakhar Shukla <prakhars.cse22@sbjit.edu.in>
2025-06-25 12:09:05 +0530

    Update firebaseConfig.js

--- crypto-trader/src/config/firebaseConfig.js
+++ crypto-trader/src/config/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBZla1ZxMtgRYcb_dPRjbR0yo41i6cUrhc",
-  authDomain: "crypto-hunter-aef80.firebaseapp.com",
-  projectId: "crypto-hunter-aef80",
-  storageBucket: "crypto-hunter-aef80.firebasestorage.app",
-  messagingSenderId: "990678404093",
-  appId: "1:990678404093:web:f8f5089dec46e71e8ee794"
+  apiKey: "add you credentials",
+  authDomain: "add you credential",
+  projectId: "add you credential",
+  storageBucket: "add you credential",
+  messagingSenderId: "add you credential",
+  appId: "add you credential"
 };
 
 export default firebaseConfig;
